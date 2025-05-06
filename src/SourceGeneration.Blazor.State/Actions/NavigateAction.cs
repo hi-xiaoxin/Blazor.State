6d7da03e5ad9a0725dec7b0949a3b2cd7cb28a80
肖昕 <xiao.xin@outlook.com>
2025-05-06 13:26:07 +0800

    remove NavigateAction

--- src/SourceGeneration.Blazor.State/Actions/NavigateAction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SourceGeneration.Blazor;
-
-public class NavigateAction
-{
-    public string? Uri { get; init; }
-    public IReadOnlyDictionary<string, object?>? QueryParameters { get; init; }
-    public bool? ForceLoad { get; init; }
-    public bool? Replace { get; init; }
-}
