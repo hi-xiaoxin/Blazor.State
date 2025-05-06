eb9e9d82b3fb337b34c2086b5b7d8bdda7ba488c
肖昕 <xiao.xin@outlook.com>
2025-05-06 13:27:16 +0800

    add default .ctor

--- src/SourceGeneration.Blazor.State/StateLayoutComponentBase.cs
+++ src/SourceGeneration.Blazor.State/StateLayoutComponentBase.cs
@@ -4,5 +4,7 @@ namespace SourceGeneration.Blazor;
 
 public abstract class StateLayoutComponentBase : StateComponentBase
 {
+    public StateLayoutComponentBase() { }
+
     [Parameter] public RenderFragment? Body { get; set; }
 }
